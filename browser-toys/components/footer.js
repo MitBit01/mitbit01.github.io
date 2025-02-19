b2a9c3eca0cbe2bcd7e42dda57f53b79040bcac5
MitBit01 <mitbit01@gmail.com>
2025-02-19 11:15:13 -0500

    create new project format

--- browser-toys/components/footer.js
+++
@@ -1,7 +0,0 @@
-import { m } from '../util/lib.js'
-
-export default function Footer() {
-  return {
-    view: () => m('footer.container', m('a', {onclick:() => window.scrollTo({top: 0, left: 0, behavior: 'smooth'})}, '[Top]'))
-  }
-}
--- browser-toys/util/lib.js
+++
@@ -1,7 +0,0 @@
-// @ts-ignore
-import m from 'https://cdn.jsdelivr.net/npm/mithril/+esm'
-
-// @ts-ignore
-import monacoEditor from 'https://cdn.jsdelivr.net/npm/monaco-editor/+esm'
-
-export { m, monacoEditor }
