b62df779e2d7403795542f7ccaec9ec1551f04c3
MitBit01 <mitbit01@gmail.com>
2026-04-29 14:35:53 -0500

    change minserdes to deflate-raw

--- libs/minserdes.js
+++ libs/minserdes.js
@@ -2,14 +2,14 @@ import JsonURL from 'https://cdn.jsdelivr.net/npm/@jsonurl/jsonurl/+esm'
 
 export async function serialize(val) {
   const str = JsonURL.stringify(val, { AQF: true, noEmptyComposite: true })
-  const cmp = await compress(str, 'brotli')
+  const cmp = await compress(str, 'deflate-raw')
   const enc = cmp.toBase64({ alphabet: 'base64url', omitPadding: true })
   return enc
 }
 
 export async function deserialize(enc) {
   const cmp = Uint8Array.fromBase64(enc, { alphabet: 'base64url' })
-  const str = await decompress(cmp, 'brotli')
+  const str = await decompress(cmp, 'deflate-raw')
   const val = JsonURL.parse(str, { AQF: true, noEmptyComposite: true })
   return val
 }
